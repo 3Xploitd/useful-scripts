d90530d22821c9872a6b4adc7c45493603b2c86c
3Xploitd <40121116+3Xploitd@users.noreply.github.com>
2023-11-01 01:46:29 -0500

    Update Fuzz_endpoint.ps1

--- Fuzz_endpoint.ps1
+++ Fuzz_endpoint.ps1
@@ -5,9 +5,11 @@ param(
 $fuzz_values = gc $file
 
 foreach ($obj in $fuzz_values) {
-    $response = iwr -uri $url/$obj -ErrorAction ContinueSilently
-    if ($response.StatusCode -ne $Null){
-    Write-Host "Response from $url/$obj"
-    Write-Host $response.RawContent
+    Try {
+    $response = iwr -uri $url/$obj
+    $response.RawContent
+
+    } Catch {
+
     }
 }
